09daced2f7af7438f3fc3d78e478e41803193394
HS211777 <HS211777@reigate.ac.uk>
2021-11-16 11:59:11 +0000

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Random;
 
 public class Main {
 
@@ -15,16 +14,29 @@ public class Main {
     public static void main(String[] args) {
         createNumsArray();
         boolean end = false;
-        while(!end){
+        while (!end) {
             mainmenu();
-            int choice = Integer.parseInt(input());
-            switch (choice){
+            int choice = 0;
+            try{
+                boolean valid = false;
+                while(!valid){
+                    choice = Integer.parseInt(input());
+                    valid = true;
+                }
+            }
+            catch (Exception e){
+                System.out.println("Error "+e);
+            }
+            switch (choice) {
                 case 1:
                     buyTickets();
                     break;
                 case 2:
-                    if (checkTickets()){
-                        System.out.println("you win a prize");
+                    if (checkTickets()) {
+                        System.out.println("You win a prize");
+                    }
+                    else{
+                        System.out.println("You do not win");
                     }
                     break;
                 case 3:
@@ -35,74 +47,117 @@ public class Main {
             }
         }
     }
-    public static void createNumsArray(){
-        for (int i = 0; i < 500; i++){
-            nums.add(i+1);
+
+    public static void createNumsArray() {
+        for (int i = 0; i < 500; i++) {
+            nums.add(i + 1);
         }
         Collections.shuffle(nums);
     }
-    public static void buyTickets(){
+
+    public static void buyTickets() {
         boolean valid = false;
-        while (!valid){
-            try{
+        while (!valid) {
+            try {
                 System.out.println("Enter your Name");
                 String name = input();
                 names.add(name);
                 valid = true;
-            }
-            catch(Exception e){
-                System.out.println("Error "+e);
+            } catch (Exception e) {
+                System.out.println("Error " + e);
             }
         }
+        System.out.println("your number is: "+nums.get(0));
         tickets.add(nums.get(0));
         nums.remove(0);
     }
-    public static void mainmenu(){
+
+    public static void mainmenu() {
         System.out.println("Menu:");
         System.out.println("1 - Buy Ticket");
         System.out.println("2 - Check Tickets");
         System.out.println("3 - Exit");
     }
-    public static boolean checkTickets(){
+
+    public static boolean checkTickets() {
         System.out.println("Enter your name:");
-        String name = input()
+        String name = input();
         System.out.println("Enter ticket number:");
-        int ticketNum = Integer.parseInt(input());
+        int ticketNum = 0;
+        try{
+            boolean valid = false;
+            while (!valid){
+                ticketNum = Integer.parseInt(input());
+                valid = true;
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error "+e);
+        }
         boolean validName = false;
         int nameKey = -1;
         boolean validNum = false;
         int numkey = -1;
-        for (int i = 0; i < names.size(); i++){
-            if (names.get(i).equalsIgnoreCase(name)){
+        for (int i = 0; i < names.size(); i++) {
+            if (names.get(i).equalsIgnoreCase(name)) {
                 validName = true;
                 nameKey = i;
             }
         }
-        for (int i = 0; i < tickets.size(); i++){
-            if (tickets.get(i) == ticketNum){
+        for (int i = 0; i < tickets.size(); i++) {
+            if (tickets.get(i) == ticketNum) {
                 validNum = true;
                 numkey = i;
             }
         }
-        boolean valid = false;
-        if (validName && validNum){
-            if (numkey == nameKey){
-                System.out.println("Your name and ticekt number are correct");
-                valid = true;
-            }
-            else{
-                System.out.println("either your name and ticket number do not match");
-            }
-        }
-        else{
-            System.out.println("either your name or ticket number is not on our database");
-        }
-        
+        return isPrime(numkey,verifyTicket(validName,validNum,nameKey,numkey));
     }
+
     public static String input(){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        return br.readLine();
+        boolean valid = false;
+        String out = "";
+        try{
+            while (!valid){
+                out = br.readLine();
+                valid = true;
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error "+e);
+        }
+        return out;
     }
-    //store names and raffle numbers in 2 lists
+    public static boolean isPrime(int numkey, boolean valid){
+        boolean prime = false;
+        if (valid) {
+            if (tickets.get(numkey) != 1) {
+                for (int i = 2; i < tickets.get(numkey); i++) {
+                    if (tickets.get(numkey) % i == 0) {
+                        prime = false;
+                        break;
+                    }
+                    else{
+                        prime = true;
+                    }
+                }
+            }
+        }
+        return prime;
+    }
+    public static boolean verifyTicket(boolean validName, boolean validNum, int nameKey, int numkey){
+        boolean valid = false;
+        if (validName && validNum) {
+            if (numkey == nameKey) {
+                System.out.println("Your name and ticket number are correct");
+                valid = true;
+            } else {
+                System.out.println("Your name and ticket number do not match");
+            }
+        } else {
+            System.out.println("Either your name or ticket number is not on our database");
+        }
+        return valid;
+    }
     //if raffle number is prime the person won a prize
 }
